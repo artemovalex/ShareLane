b35599d7cb4c210832d0454d637b5138b379dc88
testershmester <katerina.zharskaya@mail.ru>
2022-06-20 21:52:19 +0300

    SHRL-154_Refactoring: Added WebDriverManager

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -1,22 +1,22 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
 
 public class BaseTest {
     WebDriver driver;
 
-    @BeforeClass
-    public void setPathToWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-    }
-
     @BeforeMethod
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
     }
 
